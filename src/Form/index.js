db74856596a9767d03ad65ed23fd5c32682cbf83
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-10-12 14:42:11 +0200

    class 2.2 selecting form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, /* MainSpace, */ FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
-/* import DatosPersonales from "./DatosPersonales";
+import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
-import Stepper from "../Stepper"; */
+import Stepper from "../Stepper";
 
 const Form = () => {
+
+  const [step, setStep] = useState(0)
+
+  const steps = {
+       0:  <DatosUsuario />,
+       1:  <DatosPersonales />,
+       2:  <DatosEntrega />,
+       3:  <Complete />,
+      };
+
   return (
     <Box
       sx={{
@@ -21,9 +31,11 @@ const Form = () => {
         <Typography variant="h3">AluraFood</Typography>
       </LogoSpace>
       <FormSpace>
-        <DatosUsuario />
-{/*         <DatosPersonales />
+        { step < 3 && <Stepper step={step} />}
+        {/*<DatosUsuario />
+         <DatosPersonales />
         <DatosEntrega /> */}
+        {steps[step]}
       </FormSpace>
     </Box>
   );
